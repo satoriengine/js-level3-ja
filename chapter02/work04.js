6257f451501d9cf45c4d4c23fe2bca492dc26086
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2026-01-20 08:48:46 +0000

    fix chapter02

--- chapter02/work04.js
+++ chapter02/work04.js
@@ -3,6 +3,7 @@ let color = 'red';
 if (color == 'red') {
     // ブロックスコープの変数の宣言
     let message = 'This is red';
+    console.log(message);
 }
 
 // ブロック外なので利用不可
--- chapter02/work05.js
+++ chapter02/work05.js
@@ -1,6 +1,7 @@
 for (let i = 0; i < 3; i++) {
     //ブロックスコープの変数の宣言
     let message = 'Hello';
+    console.log(message);
 }
 
 // ブロック外なので利用不可
